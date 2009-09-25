f05c9172a9ca2ce123caa0bb2cad2ff630fcf32d
buchholz.bastian <buchholz.bastian@d697556a-8733-11de-9d35-41d669e99a42>
2009-09-25 13:03:29 +0000

    [lifedb] Some css fixes. Added some docs. Added a lifedb logo with transparent background. Change TabPanels dynamically.

--- trunk/lifedb/src/java/de/dkfz/mga/lifedb/CloneSearch.java
+++
@@ -1,9 +0,0 @@
-package de.dkfz.mga.lifedb;
-
-public class CloneSearch {
-
-    /** Creates a new instance of CloneSearch */
-    public CloneSearch() {
-    }
-
-}
--- trunk/lifedb/src/java/de/dkfz/mga/lifedb/TabManager.java
+++ trunk/lifedb/src/java/de/dkfz/mga/lifedb/TabManager.java
@@ -1,112 +1,35 @@
 package de.dkfz.mga.lifedb;
 
-import javax.faces.component.UIPanel;
 import javax.faces.event.ActionEvent;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.io.Serializable;
 
 public class TabManager implements Serializable {
 
-    private int tabIndex = 0;
-    private List tabs = new ArrayList();
+    private int activeTabPanel = 0;
 
-    public TabManager() {
-        Tab t1 = new Tab("asdf 1", new UIPanel(), 0);
-        this.add(t1);
-        Tab t2 = new Tab("asdf 2", new UIPanel(), 1);
-        this.add(t2);
+    public void showWelcome(ActionEvent e) {
+        this.setActiveTabPanel(0);
     }
 
-    public void add(Tab tab) {
-        this.tabs.add(tab);
+    public void showSearchClones(ActionEvent e) {
+        this.setActiveTabPanel(1);
     }
 
-    public void add(Tab[] tabs) {
-        for (Tab tab : tabs) {
-            this.add(tab);
-        }
+    public void showSearchPathways(ActionEvent e) {
+        this.setActiveTabPanel(2);
     }
 
-    public void remove(Tab tab) {
-        this.tabs.remove(tab);
+    public void showSearchSubcellularLocations(ActionEvent e) {
+        this.setActiveTabPanel(3);
     }
 
-    public void removeAll() {
-        this.tabs.clear();
-        this.tabIndex = 0;
+    public void setActiveTabPanel(int index) {
+        this.activeTabPanel = index;
     }
 
-    public List getTabs() {
-        return this.tabs;
-    }
-
-    public int getTabsSize(){
-        return this.tabs.size();
-    }
-
-    public int getTabIndex() {
-        return this.tabIndex;
-    }
-
-    public void setTabIndex(int tabIndex) {
-        this.tabIndex = tabIndex;
-    }
-
-    public class Tab {
-
-        private String label;
-        private UIPanel content;
-        private int index;
-        private boolean removable = false;
-
-        public Tab() {
-
-        }
-
-        public Tab(String label, UIPanel content, int index) {
-            this.setLabel(label);
-            this.setContent(content);
-            this.setIndex(index);
-        }
-
-        public String getLabel() {
-            return this.label;
-        }
-
-        public UIPanel getContent() {
-            return this.content;
-        }
-
-        public int getIndex() {
-            return this.index;
-        }
-
-        public boolean isRemovable() {
-            return this.removable;
-        }
-
-        public void setLabel(String label) {
-            this.label = label;
-        }
-
-        public void setContent(UIPanel content) {
-            this.content = content;
-        }
-
-        public void setIndex(int index) {
-            this.index = index;
-        }
-
-        public void setRemovable(boolean removable) {
-            this.removable = removable;
-        }
-
-        public void closeTab(ActionEvent event) {
-           TabManager.this.tabs.remove(this);
-        }
-
+    public int getActiveTabPanel() {
+        return this.activeTabPanel;
     }
 
 }
