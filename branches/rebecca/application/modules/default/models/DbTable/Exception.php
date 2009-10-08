34203d4d5aaf3cc9a61a09d9b072f172ccc45bc6
buchholz.bastian <buchholz.bastian@d697556a-8733-11de-9d35-41d669e99a42>
2009-10-08 18:20:10 +0000

    Delete [tags] and [branches].

--- branches/rebecca/application/modules/default/models/DbTable/Exception.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-class Application_Model_DbTable_Exception extends Exception{}
--- branches/rebecca/library/Application/Controller/AjaxInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface Application_Controller_AjaxInterface
-{
-
-    const CONTEXT_JSON = 'json';
-    const CONTEXT_XML = 'xml';
-
-}
