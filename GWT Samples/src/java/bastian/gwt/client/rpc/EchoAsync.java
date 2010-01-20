9b2f27ed96233de360c7a49808aa8f7a7aca8232
buchholz.bastian <buchholz.bastian@d697556a-8733-11de-9d35-41d669e99a42>
2010-01-20 13:01:47 +0000

    [GWT Samples] removed!!!

--- GWT Samples/src/java/bastian/gwt/client/rpc/EchoAsync.java
+++
@@ -1,9 +0,0 @@
-package bastian.gwt.client.rpc;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface EchoAsync {
-
-  public void echo(String s, AsyncCallback<String> callback);
-
-}
