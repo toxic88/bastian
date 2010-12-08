16eb3d73d177c38ee642cb4d79d2b322cdfe0016
buchholz.bastian <buchholz.bastian@d697556a-8733-11de-9d35-41d669e99a42>
2010-12-08 17:57:06 +0000

    adsense now working robots.txt added

--- bastian-buchholz/src/de/bastian/index/client/AppView.java
+++ bastian-buchholz/src/de/bastian/index/client/AppView.java
@@ -5,21 +5,26 @@ import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class AppView extends Composite {
 
     private static AppViewUiBinder uiBinder = GWT.create(AppViewUiBinder.class);
 
-    interface AppViewUiBinder extends UiBinder<Widget, AppView> {
-    }
+    interface AppViewUiBinder extends UiBinder<Widget, AppView> {}
 
     public AppView() {
         initWidget(uiBinder.createAndBindUi(this));
+
+        ads.appendChild(RootPanel.get("ads").getElement());
     }
 
     @UiField
     DivElement content;
+
+    @UiField
+    DivElement ads;
 
     public DivElement getContent() {
         return content;
