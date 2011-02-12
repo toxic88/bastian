d5d8854bb3330c0a55a8603606430cbc7225e18f
buchholz.bastian <buchholz.bastian@d697556a-8733-11de-9d35-41d669e99a42>
2011-02-12 20:27:49 +0000

    added URL support as specified here: http://dev.w3.org/2006/webapi/FileAPI/#url
    also added the new sdk versions of appengine and gwt

---
+++ FileApi/src/de/bastian/gwt/fileapi/client/file/URL.java
@@ -0,0 +1,34 @@
+package de.bastian.gwt.fileapi.client.file;
+
+/**
+ * http://www.w3.org/TR/FileAPI/#creating-revoking
+ * @author Bastian Buchholz
+ */
+public class URL {
+
+    /**
+     * Checks if the browser supports the URL File API
+     * @return boolean
+     */
+    public final static native boolean isSupported() /*-{
+        return !!$wnd.URL && !!$wnd.URL.createObjectURL;
+    }-*/;
+
+    /**
+     * http://www.w3.org/TR/FileAPI/#dfn-createObjectURL
+     * @param blob
+     * @return String
+     */
+    public final static native String createObjectURL(Blob blob) /*-{
+        return $wnd.URL.createObjectURL(blob);
+     }-*/;
+
+    /**
+     * http://www.w3.org/TR/FileAPI/#dfn-revokeObjectURL
+     * @param url
+     */
+    public final static native void revokeObjectURL(String url) /*-{
+        $wnd.URL.revokeObjectURL(url);
+    }-*/;
+
+}
--- FileApi/src/de/bastian/gwt/fileapi/sample/client/FileApi.java
+++ FileApi/src/de/bastian/gwt/fileapi/sample/client/FileApi.java
@@ -4,6 +4,8 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.LoadEvent;
+import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
@@ -13,6 +15,7 @@ import de.bastian.gwt.fileapi.client.file.File;
 import de.bastian.gwt.fileapi.client.file.FileReader;
 import de.bastian.gwt.fileapi.client.file.FileReaderListener;
 import de.bastian.gwt.fileapi.client.file.ProgressEvent;
+import de.bastian.gwt.fileapi.client.file.URL;
 import de.bastian.gwt.fileapi.client.ui.FileUpload;
 
 /**
@@ -32,44 +35,57 @@ public class FileApi implements EntryPoint {
         upload.addChangeHandler(new ChangeHandler() {
             @Override
             public void onChange(ChangeEvent event) {
+                panel.clear();
                 if (FileReader.isSupported()) { // check if the FileReader is supported by your browser
                     for (int i = 0; i < upload.getFiles().length(); i++) { // loop through a selected files
                         final File file = upload.getFiles().get(i);
-
-                        FileReader fileReader = new FileReader(new FileReaderListener() {
-                            @Override
-                            public void onProgress(ProgressEvent event) {
-                                // calculate how much % is already read
-                                GWT.log("onProgress: " + Math.round((event.getLoaded() * 100) / event.getTotal()) + "%");
+                        if (URL.isSupported()) {
+                            if (file.getType().startsWith("image/")) {
+                                final Image img = new Image(URL.createObjectURL(file)); // for displaying just the image this method is enough if it is supported
+                                img.addLoadHandler(new LoadHandler() {
+                                    @Override
+                                    public void onLoad(LoadEvent event) {
+                                        URL.revokeObjectURL(img.getUrl()); // releasing memory
+                                    }
+                                });
+                                panel.add(img);
                             }
-                            @Override
-                            public void onLoadEnd(ProgressEvent e) {
-                                GWT.log("onLoadEnd");
-                                String result = (String) getFileReader().getResult(); // the base64 encoded file contents
-                                if (result.startsWith("data:image")) { // check if its an image
-                                    panel.add(new Image(result)); // create an image and show the file!
+                            panel.add(new Label(file.getName()));
+                        } else {
+                            FileReader fileReader = new FileReader(new FileReaderListener() {
+                                @Override
+                                public void onProgress(ProgressEvent event) {
+                                    // calculate how much % is already read
+                                    GWT.log("onProgress: " + Math.round((event.getLoaded() * 100) / event.getTotal()) + "%");
                                 }
-                                panel.add(new Label(file.getName())); // show the name of the file
-                            }
-                            @Override
-                            public void onLoadStart(ProgressEvent event) {
-                                GWT.log("onLoadStart");
-                                panel.clear();
-                            }
-                            @Override
-                            public void onAbort(ProgressEvent event) {
-                                GWT.log("onAbort");
-                            }
-                            @Override
-                            public void onError(ProgressEvent event) {
-                                GWT.log("onError: " + getFileReader().getError().getCode());
-                            }
-                            @Override
-                            public void onLoad(ProgressEvent event) {
-                                GWT.log("onLoad");
-                            }
-                        });
-                        fileReader.readAsDataURL(file); // read the file! it will be base64 encoded
+                                @Override
+                                public void onLoadEnd(ProgressEvent e) {
+                                    GWT.log("onLoadEnd");
+                                    String result = (String) getFileReader().getResult(); // the base64 encoded file contents
+                                    if (result.startsWith("data:image")) { // check if its an image. you can also use file.getType()
+                                        panel.add(new Image(result)); // create an image and show the file!
+                                    }
+                                    panel.add(new Label(file.getName())); // show the name of the file
+                                }
+                                @Override
+                                public void onLoadStart(ProgressEvent event) {
+                                    GWT.log("onLoadStart");
+                                }
+                                @Override
+                                public void onAbort(ProgressEvent event) {
+                                    GWT.log("onAbort");
+                                }
+                                @Override
+                                public void onError(ProgressEvent event) {
+                                    GWT.log("onError: " + getFileReader().getError().getCode());
+                                }
+                                @Override
+                                public void onLoad(ProgressEvent event) {
+                                    GWT.log("onLoad");
+                                }
+                            });
+                            fileReader.readAsDataURL(file); // read the file! it will be base64 encoded
+                        }
                     }
                 } else { // if the browser doesn't support the FileReader API show an error message
                    RootPanel.get().add(error);
